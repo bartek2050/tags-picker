5f8e6cb0c54bd5885bdd447d81dc68b9d4208e5a
bartek2050 <bartlomiej.ciastek@gmail.com>
2024-07-04 23:58:05 +0200

    add icons

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -1,11 +1,18 @@
+import { faCircleInfo, faCog, faTag, faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faMessage } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export const MainView = () => {
   return (
-    <div className="flex flex-col justify-between gap-2 shadow-xl border-gray-700 rounded ml-40 mt-40 p-4 ">
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>4</div>
-      <div>5</div>
+    <div
+      className="flex flex-col justify-between items-center gap-8 shadow border-solid
+      border-2 border-gray-800/10 rounded ml-40 mt-40 px-6 py-4 w-5"
+    >
+      <FontAwesomeIcon icon={faCircleInfo} size="xl" />
+      <FontAwesomeIcon icon={faMessage} size="xl" />
+      <FontAwesomeIcon icon={faCog} size="xl" />
+      <FontAwesomeIcon icon={faTag} size="xl" />
+      <FontAwesomeIcon icon={faUserPlus} size="xl" />
     </div>
   );
 };
