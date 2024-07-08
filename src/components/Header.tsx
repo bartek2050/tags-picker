7ca9d3d4252e6195260d8346a51e88e0683caf35
bartek2050 <bartlomiej.ciastek@gmail.com>
2024-07-08 16:01:23 +0200

    tags header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+
+export const Header = ({ header }: { header: string }) => {
+  return (
+    <div className="flex justify-between">
+      <h1 className="h-1 font-bold">{header}</h1>
+      <div><FontAwesomeIcon icon={faX} size="sm" /></div>
+    </div>
+
+  );
+};
--- src/components/Tags/TagsSearchWrapper.tsx
+++ src/components/Tags/TagsSearchWrapper.tsx
@@ -1,7 +1,9 @@
+import { Header } from "../Header";
+
 export const TagsSearchWrapper = () => {
   return (
-    <div className="shadow border-solid border-2 border-gray-800/10 rounded">
-      Tagi
+    <div className="shadow border-solid border-2 border-gray-800/10 rounded p-3 w-20 min-w-48">
+      <Header header={"Tagi"} />
     </div>
 
   );
