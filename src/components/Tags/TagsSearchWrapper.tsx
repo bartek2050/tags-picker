05ed4eb7ca2360e1a1e774d8146085b15d4bde36
bartek2050 <bartlomiej.ciastek@gmail.com>
2024-07-08 15:53:01 +0200

    add tags wrapper

---
+++ src/components/Tags/TagsSearchWrapper.tsx
@@ -0,0 +1,8 @@
+export const TagsSearchWrapper = () => {
+  return (
+    <div className="shadow border-solid border-2 border-gray-800/10 rounded">
+      Tagi
+    </div>
+
+  );
+};
--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -2,18 +2,22 @@ import { faCircleInfo, faCog, faTag, faUserPlus } from "@fortawesome/free-solid-
 import { faMessage } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconWrapper } from "../components/IconWrapper.tsx";
+import { TagsSearchWrapper } from "../components/Tags/TagsSearchWrapper.tsx";
 
 export const MainView = () => {
   return (
-    <div
-      className="flex flex-col justify-between items-center gap-2 shadow border-solid
-      border-2 border-gray-800/10 rounded ml-40 mt-40 w-14 px-7 py-1"
-    >
-      <IconWrapper><FontAwesomeIcon icon={faCircleInfo} size="xl" /></IconWrapper>
-      <IconWrapper><FontAwesomeIcon icon={faMessage} size="xl" /></IconWrapper>
-      <IconWrapper><FontAwesomeIcon icon={faCog} size="xl" /></IconWrapper>
-      <IconWrapper><FontAwesomeIcon icon={faTag} size="xl" /></IconWrapper>
-      <IconWrapper><FontAwesomeIcon icon={faUserPlus} size="xl" /></IconWrapper>
+    <div className="ml-40 mt-40 flex gap-2">
+      <div
+        className="flex flex-col justify-between items-center gap-2 shadow border-solid
+      border-2 border-gray-800/10 rounded w-14 px-7 py-1"
+      >
+        <IconWrapper><FontAwesomeIcon icon={faCircleInfo} size="xl" /></IconWrapper>
+        <IconWrapper><FontAwesomeIcon icon={faMessage} size="xl" /></IconWrapper>
+        <IconWrapper><FontAwesomeIcon icon={faCog} size="xl" /></IconWrapper>
+        <IconWrapper><FontAwesomeIcon icon={faTag} size="xl" /></IconWrapper>
+        <IconWrapper><FontAwesomeIcon icon={faUserPlus} size="xl" /></IconWrapper>
+      </div>
+      <TagsSearchWrapper />
     </div>
   );
 };
