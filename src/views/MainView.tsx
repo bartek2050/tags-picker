10796206322ead88432863837667e83e909f7919
bartek2050 <bartlomiej.ciastek@gmail.com>
2024-07-12 18:13:33 +0200

    add keydown action

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -3,20 +3,34 @@ import { faMessage } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { IconWrapper } from "../components/IconWrapper.tsx";
 import { TagsSearchWrapper } from "../components/Tags/TagsSearchWrapper.tsx";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const MainView = () => {
   const [showTagsSearch, setShowTagsSearch] = useState(false);
 
+  const keyDownHandler = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "F1") {
+      setShowTagsSearch((prev) => !prev);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => keyDownHandler(e as unknown as React.KeyboardEvent<HTMLDivElement>));
+
+    return () => {
+      window.removeEventListener("keydown", (e) => keyDownHandler(e as unknown as React.KeyboardEvent<HTMLDivElement>));
+    };
+  }, []);
+
   return (
     <div className="ml-40 mt-40 flex gap-2 font-arial">
       <div
-        className="flex flex-col justify-between items-center gap-2 shadow-md rounded w-14 px-7 py-1"
+        className="flex flex-col justify-between items-center gap-2 shadow-md rounded w-14 px-7 py-1 h-full"
       >
         <IconWrapper><FontAwesomeIcon icon={faCircleInfo} size="xl" /></IconWrapper>
         <IconWrapper><FontAwesomeIcon icon={faMessage} size="xl" /></IconWrapper>
         <IconWrapper><FontAwesomeIcon icon={faCog} size="xl" /></IconWrapper>
-        <button onClick={() => setShowTagsSearch(true)}>
+        <button onClick={() => setShowTagsSearch((prev) => !prev)}>
           <IconWrapper><FontAwesomeIcon icon={faTag} size="xl" /></IconWrapper>
         </button>
         <IconWrapper><FontAwesomeIcon icon={faUserPlus} size="xl" /></IconWrapper>
