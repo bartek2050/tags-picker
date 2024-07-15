a861a5b9919ca46d2ee775b12c2ba70b8b3c893b
bartek2050 <bartlomiej.ciastek@gmail.com>
2024-07-15 15:29:58 +0200

    add visual meter for tags

--- src/components/Tags/SelectionMeterWrapper.tsx
+++ src/components/Tags/SelectionMeterWrapper.tsx
@@ -1,12 +1,22 @@
 import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
 export const SelectionMeterWrapper = () => {
-  const [selectedTagsCount, setSelectedTagsCount] = useState(2);
+  const [selectedTags, setSelectedTags] = useState(["Maryla", "Przeboje", "Maryla", "Przeboje"]);
+
 
   return (
     <div className="pt-2">
-      <div className="text-lime-500 font-bold">Dobrze</div>
-      {selectedTagsCount}
+      <div className="flex flex-row">
+        <div className="text-lime-600 font-bold">Dobrze</div>
+        <div className="flex flex-row gap-0.5 items-center ml-2">
+          {selectedTags.map((index) => <div key={index} className="w-3 h-1 bg-lime-600 rounded" />)}
+        </div>
+      </div>
+      <div className="text-gray-400 text-xs flex flex-row items-center gap-2 mt-2">
+        <FontAwesomeIcon icon={faCircleInfo} size={"lg"} />
+      </div>
     </div>
 
   );
