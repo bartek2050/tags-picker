2b037c31350d6b793b495afabca1603eaaf10d00
bartek2050 <bartlomiej.ciastek@gmail.com>
2024-07-16 17:32:23 +0200

    map selected tags

--- src/components/Tags/SelectedTagsWrapper.tsx
+++ src/components/Tags/SelectedTagsWrapper.tsx
@@ -1,7 +1,9 @@
 export const SelectedTagsWrapper = () => {
+  const tags = ["Maryla", "Test", "Edek"];
+
   return (
-    <p className="p-4">
-      TAGS
+    <p className="p-4 flex flex-wrap gap-2">
+      {tags.map((e) => <p key={e}>{e}</p>)}
     </p>
   );
 };
