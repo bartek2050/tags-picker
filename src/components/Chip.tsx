aabc9650419c433371f08ece64385eeaba119549
bartek2050 <bartlomiej.ciastek@gmail.com>
2024-07-17 12:12:11 +0200

    add chip with selected tags

---
+++ src/components/Chip.tsx
@@ -0,0 +1,11 @@
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const Chip = ({ children }: { children: string }) => {
+  return (
+    <div className="border-solid rounded-lg border-gray-400 border px-2 py-1 flex flex-row gap-1 items-center">
+      <span className="font-bold">{children}</span>
+      <FontAwesomeIcon icon={faX} size="xs" className="text-gray-500" />
+    </div>
+  );
+};
--- src/components/Tags/SelectedTagsWrapper.tsx
+++ src/components/Tags/SelectedTagsWrapper.tsx
@@ -1,9 +1,11 @@
+import { Chip } from "../Chip.tsx";
+
 export const SelectedTagsWrapper = () => {
   const tags = ["Maryla", "Test", "Edek"];
 
   return (
-    <p className="p-4 flex flex-wrap gap-2">
-      {tags.map((e) => <p key={e}>{e}</p>)}
-    </p>
+    <div className="py-4 flex flex-wrap gap-2">
+      {tags.map((e) => <Chip key={e}>{e}</Chip>)}
+    </div>
   );
 };
