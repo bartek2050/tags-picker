bdbd7a7ae40162b4078da615ea15d22d2db53b4b
bartek2050 <bartlomiej.ciastek@gmail.com>
2024-08-06 10:58:58 +0200

    add TagsList component, update, prettier changes

--- src/components/Chip.tsx
+++ src/components/Chip.tsx
@@ -5,6 +5,7 @@ import { SelectedTagsContext } from "../context/SelectedTagsContext.ts";
 
 export const Chip = ({ children }: { children: string }) => {
   const { deleteTags } = useContext(SelectedTagsContext);
+
   return (
     <div className="border-solid rounded-lg border-gray-400 border px-3 py-1 flex flex-row gap-3 items-center">
       <span className="font-bold">{children}</span>
---
+++ src/components/Tags/TagsList.tsx
@@ -0,0 +1,30 @@
+import { TagsListElement } from "../Tags/TagsListElement.tsx";
+import React, { useContext, useState } from "react";
+import { SelectedTagsContext } from "../../context/SelectedTagsContext.ts";
+
+type TagsListProps = {
+  inputValue: string;
+  setInputValue: (v: string) => void
+}
+
+export const TagsList: React.FC<TagsListProps> = ({ inputValue, setInputValue }) => {
+  const [tagsToAdd, setTagsToAdd] = useState<string[]>([]);
+  const { addTags } = useContext(SelectedTagsContext);
+  const saveClickHandler = () => {
+    setInputValue("");
+    addTags(tagsToAdd);
+  };
+
+  return (
+    <div>
+      <ul className="text-sm font-medium text-gray-900 bg-white max-h-80 overflow-scroll">
+        <TagsListElement inputValue={inputValue} tagsToAdd={tagsToAdd} setTagsToAdd={(tags) => setTagsToAdd(tags)} />
+      </ul>
+      <button type="button" disabled={tagsToAdd.length === 0} className="w-full text-white bg-blue-700 hover:bg-blue-800
+      focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 me-2 my-3 focus:outline-none
+      disabled:bg-gray-300 disabled:text-gray-400" onClick={saveClickHandler}>
+        Zapisz
+      </button>
+    </div>
+  );
+};
